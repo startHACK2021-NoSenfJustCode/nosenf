5afff1e7426117c1a357309b0f77345fce5a2b4d
Twelvesky18 <marciniak.miko@gmail.com>
2021-03-20 14:38:48 +0000

    Fixed classname error

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="container">
     <Header />
-      <figure class="fluidratio"></figure>
+      <figure className="fluidratio"></figure>
       <Router>
         <Switch>
           <Route exact path="/">
@@ -38,30 +38,5 @@ function App() {
     </div >
   )
 }
-
-/*    <div className="App">
-   <Header></Header>
-  
-  <header className="App-header">
- 
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
-  <Footer></Footer>
-  <Routes/>
-</div >
-);
-}
-*/
 
 export default App;
